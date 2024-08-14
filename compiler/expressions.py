439d6b49955de38bdf98dc611b03507b2271acc6
Ryan Bemis <ryanbemis@umass.edu>
2024-08-14 12:20:32 -0400

    Began implementing ExpressionTree

--- compiler/expressions.py
+++ compiler/expressions.py
@@ -1,4 +1,13 @@
 from compiler.core import State
+from enum import Enum
+
+# TODO: Avoid redefining this in statements.py
+class ExpOp (Enum):
+    ADD = 1,
+    SUB = 2,
+    MUL = 3,
+    DIV = 4,
+    MOD = 5
 
 class Expression:
     def get_value(self, state: State) -> int:
@@ -10,3 +19,12 @@ class SingleValExpression (Expression):
 
     def get_value(self, state: State) -> int:
         return self.val
+    
+class ExpressionTree (Expression):
+    def __init__(self, operation: ExpOp, left_exp: Expression, right_exp: Expression):
+        self.left_exp = left_exp
+        self.right_exp = right_exp
+        self.operation = operation
+
+    def get_value(self, state: State):
+        pass 
