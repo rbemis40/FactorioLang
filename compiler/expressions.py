d6d44860694cf3b099f26f245f3341164ae00fcb
Ryan Bemis <ryanbemis@umass.edu>
2024-08-20 14:02:05 -0400

    Updated Expression base class for ans location

--- compiler/expressions.py
+++ compiler/expressions.py
@@ -11,6 +11,9 @@ class ExpOp (Enum):
     MOD = 5
 
 class Expression (Translatable):
+    def __init__(self):
+        self.ans_loc: int = 0 # This will be used to store the memory location of the expressions answer for use in other expressions, 0 for placeholder
+    
     @abstractmethod
     def is_computable() -> bool:
         pass
@@ -18,6 +21,9 @@ class Expression (Translatable):
     @abstractmethod
     def get_value() -> int:
         pass
+
+    def get_ans_loc(self) -> int:
+        return self.ans_loc
 
 class SingleValExpression (Expression):
     def __init__(self, val: int):
@@ -32,7 +38,7 @@ class SingleValExpression (Expression):
     def get_value(self) -> int:
         return self.val
     
-class ExpressionTree (Expression):
+class TreeExpression (Expression):
     def __init__(self, operation: ExpOp, left_exp: Expression, right_exp: Expression):
         self.left_exp = left_exp
         self.right_exp = right_exp
@@ -44,5 +50,23 @@ class ExpressionTree (Expression):
     def is_computable(self) -> bool:
         return self.left_exp.is_computable() and self.right_exp.is_computable()
 
-    def get_value(self, state: State):
-        pass
+    def get_value(self) -> int:
+        if not self.is_computable():
+            raise ValueError('Attempted to get value of non-computable expression')
+
+        left_val = self.left_exp.get_value()
+        right_val = self.right_exp.get_value()
+
+        match self.operation:
+            case ExpOp.ADD:
+                return left_val + right_val
+            case ExpOp.SUB:
+                return left_val - right_val
+            case ExpOp.MUL:
+                return left_val * right_val
+            case ExpOp.DIV:
+                return left_val // right_val
+            case ExpOp.MOD:
+                return left_val % right_val
+            case _:
+                raise Exception('Unknown expression type')
