095f1fe69613ec336749e0632478159be55112f6
Ryan Bemis <ryanbemis@umass.edu>
2024-08-22 13:44:43 -0400

    First implementation of translate for expressions

--- compiler/expressions.py
+++ compiler/expressions.py
@@ -1,4 +1,5 @@
 from compiler.core import State, Translatable, Instruction
+from compiler.instructions import SetInstruction, MathInstruction
 from enum import Enum
 from abc import ABC, abstractmethod
 
@@ -22,6 +23,9 @@ class Expression (Translatable):
     def get_value() -> int:
         pass
 
+    def set_ans_loc(self, new_loc: int) -> None:
+        self.ans_loc = new_loc
+
     def get_ans_loc(self) -> int:
         return self.ans_loc
 
@@ -30,7 +34,9 @@ class SingleValExpression (Expression):
         self.val = val
 
     def translate(self, state: State) -> list[Instruction]:
-        return [] # TODO: Implement
+        # This instruction is necessary if the containing expression is not computable, since MATH instructions require the values to be stored in memory
+        self.set_ans_loc(state.claim_mem_addr())
+        return [SetInstruction(self.get_ans_loc(), self.get_value())]
 
     def is_computable(self) -> bool:
         return True
@@ -40,12 +46,33 @@ class SingleValExpression (Expression):
     
 class TreeExpression (Expression):
     def __init__(self, operation: ExpOp, left_exp: Expression, right_exp: Expression):
-        self.left_exp = left_exp
-        self.right_exp = right_exp
-        self.operation = operation
+        self.left_exp: Expression = left_exp
+        self.right_exp: Expression = right_exp
+        self.operation: ExpOp = operation
 
     def translate(self, state: State) -> list[Instruction]:
-        return [] # TODO: Implement
+        # First we determine if this expression is computable, because if it is then we just need to store the answer into a memory location
+        if self.is_computable():
+            value = self.get_value()
+            self.set_ans_loc(state.claim_mem_addr())
+
+            return [SetInstruction(self.get_ans_loc(), value)]
+        
+        # If its not computable, then we need to first convert the two sub expressions to be stored in memory, so we can use the MATH instruction
+        
+        out_instrs: list[Instruction] = []
+
+        out_instrs.append(self.left_exp.translate())
+        out_instrs.append(self.right_exp.translate())
+
+        # At this point, the two input memory locations should be set up, now we need to set up a place to store the answer
+        self.set_ans_loc(state.claim_mem_addr())
+
+        # Now we can actually use the math expression
+        # TODO: Again, self.operation is using a different operation enum than the MathInstruction, which needs to be resolved
+        out_instrs.append(MathInstruction(self.operation, self.left_exp.get_ans_loc(), self.right_exp.get_ans_loc(), self.get_ans_loc()))
+
+        return out_instrs
 
     def is_computable(self) -> bool:
         return self.left_exp.is_computable() and self.right_exp.is_computable()
